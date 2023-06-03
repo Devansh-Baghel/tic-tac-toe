22675fc5a6b6b1081f879577bb53facdcafbc6b3
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-03 22:51:23 +0530

    Add a function to display board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-const x = "&times;";
+// const x = "&times;";
+const x = "x";
 const o = "o";
+
+const gameBoardDisplay = document.querySelector(".board");
 
 // Main gameboard obj
 let gameBoard = {
-	board: [],
+	board: [x, o, x, o],
 }
 
 // Factory for creating players
@@ -16,3 +19,19 @@ const camus = playerFactory("camus");
 const sartre = playerFactory("sartre");
 
 // Function of display board on html
+const displayBoard = () => {
+	for(i in gameBoard.board){
+		const cell = document.createElement("p");
+		gameBoardDisplay.appendChild(cell);
+		cell.classList.add(i);
+		cell.innerText = gameBoard.board[i];
+
+		if(gameBoard.board[i] === "x"){
+			cell.classList.add("x");
+		} else{
+			cell.classList.add("o");
+		}
+	}
+}
+
+displayBoard()
