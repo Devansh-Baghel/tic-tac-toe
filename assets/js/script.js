c918ff34faf911c616a3275d3493b60a008cd348
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-04 23:22:47 +0530

    Add js to check game over

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const empty = "-";
 
 const gameBoardDisplay = document.querySelector(".board");
 const infoText = document.querySelector("span");
+const infoTextHeading = document.querySelector("h1");
 
 // const playerFactory = (name, marker) => {
 	// return { name, marker };
@@ -27,7 +28,6 @@ const displayBoard = () => {
 		gameBoardDisplay.appendChild(cell);
 		cell.classList.add(i);
 		cell.innerText = game.board[i];
-		cell.classList.add("empty")
 	}
 }
 
@@ -37,14 +37,33 @@ const addMarker = (cell) => {
 	const cellIndex = cell.classList[0];
 	game.board[cellIndex] = game.turn;
 
-	if (cell.classList[1] !== "empty"){
+	if (gameBoardDisplay.classList[1] === "over"){
 		return
 	}
 
-	cell.classList.remove("empty");
+	if (cell.classList[1] === "x" || cell.classList[1] === "o"){
+		return
+	}
+
 	cell.classList.add(game.turn);
 	cell.innerText = game.turn;
 	console.table(game.board);
+
+	// Logic to check for game over
+	// This is a shitty way to do this i know... im sorry... i'll fix it later
+	if (
+		(game.board[0] === game.board[1] && game.board[1] === game.board[2] && game.board[0] !== empty && game.board[1] !== empty && game.board[2] !== empty) || // Check for horizontal win
+		(game.board[3] === game.board[4] && game.board[4] === game.board[5] && game.board[3] !== empty && game.board[4] !== empty && game.board[5] !== empty) || // Check for horizontal win
+		(game.board[6] === game.board[7] && game.board[7] === game.board[8] && game.board[6] !== empty && game.board[7] !== empty && game.board[8] !== empty) || // Check for horizontal win
+		(game.board[0] === game.board[3] && game.board[3] === game.board[6] && game.board[0] !== empty && game.board[3] !== empty && game.board[6] !== empty) || // Check for vertical win
+		(game.board[1] === game.board[4] && game.board[4] === game.board[7] && game.board[1] !== empty && game.board[4] !== empty && game.board[7] !== empty) || // Check for vertical win
+		(game.board[2] === game.board[5] && game.board[5] === game.board[8] && game.board[2] !== empty && game.board[5] !== empty && game.board[8] !== empty) || // Check for vertical win
+		(game.board[0] === game.board[4] && game.board[4] === game.board[8] && game.board[0] !== empty && game.board[4] !== empty && game.board[8] !== empty) || // Check for diagonal win
+		(game.board[2] === game.board[4] && game.board[4] === game.board[6] && game.board[2] !== empty && game.board[4] !== empty && game.board[6] !== empty) // Check for diagonal win
+	) {
+		infoTextHeading.innerText = `Game Over! ${game.turn.toUpperCase()} WON!`;
+		gameBoardDisplay.classList.add("over");
+	}
 	
 	
 	// Switch turns
@@ -52,6 +71,7 @@ const addMarker = (cell) => {
 	infoText.innerText = game.turn.toUpperCase();
 	
 };
+
 
 const cells = document.querySelectorAll("p");
 cells.forEach((cell) => {
