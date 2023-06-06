834a98c50f16a8947ea72bca3f60a78699635320
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-07 00:58:13 +0530

    Add player select modal with styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const x = "x";
 const o = "o";
 
+const playerSelectDialog = document.querySelector(".player-select");
 const gameBoardDisplay = document.querySelector(".board");
 const cells = document.querySelectorAll("p");
-
 const infoText = document.querySelector("span");
 const infoTextHeading = document.querySelector("h1");
+// playerSelectDialog.showModal();
 
 
 let game = {
